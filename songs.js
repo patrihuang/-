5f25b29ae2e43c253d63aba8d524b1b385f77d73
patrihuang <326ecnavda@gmail.com>
2026-01-04 03:38:41 +0800

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,16 @@
+// songs.js
+// 資料檔（只放資料，不寫任何邏輯）
+// 每筆都要有 len（字數）
+
+window.SONGS = [
+  { who:"媽媽", id:"918076", title:"真情放袂離", artist:"佚名", lang:"台語", page:"第071頁", len:"4" },
+  { who:"合唱", id:"917544", title:"真情放袂開", artist:"羅文聰、陳亞蘭", lang:"台語", page:"第071頁", len:"4" },
+  { who:"爸爸", id:"911356", title:"真情無後悔", artist:"龍千玉", lang:"台語", page:"第071頁", len:"5" },
+  { who:"",     id:"920833", title:"真情磅無中", artist:"鐵澄", lang:"台語", page:"第071頁", len:"1" },
+  { who:"媽媽", id:"919060", title:"真愛一萬年", artist:"良一、綾", lang:"台語", page:"第071頁", len:"5" },
+  { who:"",     id:"925178", title:"真愛歹收回", artist:"林孟宗", lang:"台語", page:"第071頁", len:"5" },
+  { who:"哥哥", id:"912880", title:"真愛只有你", artist:"傅振輝、林姍", lang:"台語", page:"第071頁", len:"5" },
+  { who:"妹妹", id:"912614", title:"真愛無後悔", artist:"蔡小虎、陳美鳳", lang:"台語", page:"第071頁", len:"5" }
+
+  // 👉 你後面直接繼續貼 20000 筆在這裡
+];
